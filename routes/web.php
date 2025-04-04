2ad0a0bba3ca58404faba6121673d847099638d1
Rupesh Thapa <thaparupesh767@gmail.com>
2025-04-04 15:33:36 +0545

    setting up auth using bootstrap

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return "Hello, World!";
-    
+Route::get('/', function () {
+    return view('auth.login');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
